744ff646ea95be50c4e10f8aeca8850b3184c046
Gudimetla Srikanth <117501838+gudimetla-srikanth@users.noreply.github.com>
2024-02-20 11:57:48 +0530

    Update inputTaking.rb

--- inputTaking.rb
+++ inputTaking.rb
@@ -3,5 +3,5 @@
 #taking input in ruby
 puts "enter your name:"
 first = gets.chomp
-#input val always in string format.So,using to_i method convert input to integer
+#input val always in string format.So,using to_i method to convert input to integer
 puts first.to_i
