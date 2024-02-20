a8339b47fdd3d0effffa343a6f061044aa55514d
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-02-20 11:44:10 +0530

    numnbers

---
+++ numbers.rb
@@ -0,0 +1,22 @@
+
+#working with numbers in ruby
+
+ first_val = 23
+ second_val = 34
+
+#addition
+ puts first_val + second_val
+
+#multiplication
+ puts first_val * 5
+
+#division
+puts 20 / 10  #output 2
+
+#modulation for getting remainder 
+puts 20 % 10   #output 0
+
+#multiplication of strings
+
+#output will come as "5555555555"
+ puts "55" * 5
