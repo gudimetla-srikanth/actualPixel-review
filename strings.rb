ed6918bea41a19a5d286d8e173728f6155c1b7f0
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-02-20 11:24:11 +0530

    first commit

---
+++ strings.rb
@@ -0,0 +1,16 @@
+
+#Ruby programming language is dynamic means no need to assign types before using.
+puts "welcome to ruby world"
+
+first_name = "srikanth"
+last_name = "gudimetla"
+
+puts first_name + last_name
+puts first_name + " " + last_name
+
+# The difference between "puts" and "print" is "print" prints evrything in one line and "puts" prints one statement in only one line.
+print first_name
+print last_name
+p first_name
+
+#string interpolation 
