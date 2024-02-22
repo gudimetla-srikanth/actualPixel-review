775c1ffedc717601d266ccae39059bdb338eda8b
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-02-22 12:36:15 +0530

    committed

---
+++ general_practice_file.rb
@@ -0,0 +1,40 @@
+
+# a = "srikanth"
+# puts a.slice(1,4)   #output gives rika
+
+# puts a.match?('sri')  # output is true
+
+# puts a.include?('s')  #outputs true
+
+#global variables
+# name = "srikanth"
+# def creating
+#    puts name  #gives error
+# end
+# creating
+
+# $name  = "srikanth"
+# def creating
+#   puts $name
+# end
+# creating
+
+#class variables
+
+# class Actual
+#   @@name = "srikanth"
+# end
+# puts Actual.class_variable_get(:@@name)  #one way of accessing class variable
+
+#iterators
+
+# 20.times { |val| print val," "}
+# (1..20).step(5) {|val| print val," "}    #it skips 5 steps
+
+#runs infinity times
+# while 2<3 do
+#   print "hi"
+# end
+
+
+
