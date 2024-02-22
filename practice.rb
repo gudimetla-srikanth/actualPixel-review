07c7dbc794eec638d5a1895f3e8218fcea639752
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-02-22 08:04:03 +0530

    committed

---
+++ practice.rb
@@ -0,0 +1,45 @@
+
+# for i in 1..5
+#   puts i
+# end
+# ar = [1,6,3,12]
+# print ar.sort
+
+#inheritance.Ruby does not support multiple inheritance due to availabilty of modules.
+# class Student
+#   def print
+#     puts "I am parent Printing method"
+#   end 
+
+#   def write
+#     puts "I am wring method"
+#   end
+# end
+# class ChildStudent < Student
+#   def print
+#     super()
+#     puts "I am child printing method"
+#   end
+# end
+
+# child_student_obj = ChildStudent.new
+# puts child_student_obj.print
+
+# 20.times { |val| puts val}
+
+#program to find longest string
+
+# string_array = ["srikanth","srinivas","ramanujan","elonmusk","narendra modi"]
+# length = 0
+# string = ""
+# for text in string_array
+#   new_length = text.length
+#   if new_length > length
+#     string = text
+#     length = new_length
+#   end
+# end
+# puts string
+# 20.times {print"-"}
+# puts ""
+# puts string.reverse
