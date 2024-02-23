74fb19ed032e1df61d4686e22a0dd9eb9ddd67f4
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-02-23 19:28:31 +0530

    classes and mixins working file

---
+++ classesMixinsModules.rb
@@ -0,0 +1,21 @@
+
+#This file comprises of modules and mixins and classes
+
+# module ParentModule
+#   name = "srikanth"
+#   def printing
+#     puts "I am printing method from module"
+#   end
+# end
+# class Student
+
+#   #This below code makes the availability of module in classes this is generally called as mixin.
+#   include ParentModule
+#   def to_s
+#     # name1 = ParentModule::name   #gives nothing no error but outputs module name
+#   end
+# end
+
+# student_obj = Student.new
+# puts student_obj
+# puts student_obj.printing    #This line of code access method from module and prints the appropriate code.
--- tempCodeRunnerFile.rb
+++ tempCodeRunnerFile.rb
@@ -1,1 +1,8 @@
-@@name2 = "ratan tata"
+class Student
+
+#   #This below code makes the availability of module in classes this is generally called as mixin.
+#   include ParentModule
+# end
+
+# student_obj = Student.new
+# puts student_obj
