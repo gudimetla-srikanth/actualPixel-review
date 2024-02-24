0ecb4d4c514a00a80a90884960bbec10e8e8b0b5
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-02-24 13:37:14 +0530

    variable accessing in modules,classes

--- variable_accessing.rb
+++ variable_accessing.rb
@@ -1,59 +1,37 @@
 
-#This code works as expected
+
+# instance variables
+# module Student
+#   @name = "srikanth"
+#   def Student.print
+#     puts @name
+#   end 
+# end
+# Student.print       #gives output as "srikanth"
 
 # @name = "srikanth"
-# puts @name
-
-# module Actual
-#   @name = "srikanth"
-#   @@data = "srikanth gudimetla"
-#   def self.printing
-#     puts @name
-#     puts @@data
-#   end
-#   # self.printing          #to access method within the module you need to write module before the method name and access it using "." operator
+# def print
+#   puts @name
 # end
-# Actual.printing   
-# Actual::@name     #error while accessing instance variable
-# Actual::@@data   #error while accessing class variable
+# print       #this gives output as "srikanth"
 
 #class variables
-# class Student
-#   puts "class started"   #this code is executed
+
+# module Student
+#   @@name = "actual"
+#   def Student.print
+#     puts @@name
+#   end
 # end
+# Student.print         #this gives output "actual"
 
-class Student
-
-  #This code works
-  # @@name = "srikanth"
-  # puts @@name   
-  # @name = "actual"
-  # puts @name
-  # Name="srikanth"
-  # puts Name
-end
-# obj = Student.new
+# class Student
+#   @name = "srikanth"
+#   def self.print
+#     puts @name
+#   end
+# end
+# obj=Student.new 
+# Student.print
 
 
-#to access module classes,methods and general variables and constants syntax is defined bellow
-
-# constants
-#module name::constant variable name
-
-#general variables
-#module name:: variable name
-
-#classes
-#module name::class name
-
-#methods
-#module name.method name  (method should has module name prefixed with it is original name in the module also)
-
-
-module DataOne
-  def DataOne.print
-    puts "hi"
-   return @@name = "srikanth"   #does not give value
-  end
-end
-DataOne.print
