3ab4e2cf34a43f457007d0579201c9a22ae61971
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-02-25 16:38:54 +0530

    added about an d index pages

---
+++ alpha-blog/app/controllers/article_controller.rb
@@ -0,0 +1,6 @@
+class ArticleController < ApplicationController
+  def index
+  end
+  def about
+  end
+end
--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "article#index"
+  get "/about",to:"article#about"
 end
