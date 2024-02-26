0149a64f34f83e138ef2eaa0c7f3af8390d18172
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-02-26 08:47:39 +0530

    symbols uses

---
+++ symbols.rb
@@ -0,0 +1,31 @@
+
+#symbols are like strings but not strings
+=begin
+strings work with data but symbols are identifiers and are unique.
+symbols are used to represent identifiers
+symbols are used in methods,classes mainly
+=end
+
+# str = "srikanth"
+# sym = :str
+# puts "This is string val : #{str}"  #It gives output "srikanth"
+# puts "This is symbol val : #{sym}"   #it gives output str
+# puts sym.class    #it gives ouput as "Symbol"
+
+#strings create different object memory locations when they are assigned to different variables
+#symbols does not create they are uniques.same symbol assigned to different variables will point to same memory location.
+# str = "srikanth"
+# str1 = "srikanth"
+# sym = :srikanth
+# sym1 = :srikanth
+
+# #gives different object id's
+# puts str.object_id
+# puts str1.object_id
+# #gives same object id's
+# puts sym.object_id
+# puts sym1.object_id
+
+#symbols in classes are used to define instance variables
+#symbols are very fast compared to strings
+
