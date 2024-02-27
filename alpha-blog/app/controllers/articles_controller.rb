42aea5eb3d08bd78aabba8c6a86deda892208cbb
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-02-27 15:45:12 +0530

    added CRUD operations to app

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -7,10 +7,25 @@ class ArticlesController < ApplicationController
   end
   def new
   end
+  def edit 
+    @article = Article.find(params[:id])
+  end
+  def update 
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
   def create
     @article = Article.new(params.require(:article).permit(:title,:description))
     @article.save 
     redirect_to @article
   end
-  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to "/articles"
+  end
 end
