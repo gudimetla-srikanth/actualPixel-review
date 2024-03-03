e390d232fbe6897b9756d882cd6d2ed1bbecb947
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-03 12:31:25 +0530

    associations

---
+++ test-app/app/models/aticle.rb
@@ -0,0 +1,3 @@
+class Aticle < ApplicationRecord
+  blongs_to :user
+end
---
+++ test-app/test/models/aticle_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AticleTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
