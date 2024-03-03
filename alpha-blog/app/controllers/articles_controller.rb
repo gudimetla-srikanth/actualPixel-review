30febce5ce58ae087ce85594a6beb78abed1e12e
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-03 09:01:24 +0530

    added byebug and updated feature to our app

--- alpha-blog/app/controllers/articles_controller.rb
+++ alpha-blog/app/controllers/articles_controller.rb
@@ -1,4 +1,22 @@
 class ArticlesController < ApplicationController
   def index 
+    @article = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title,:description)) 
+      redirect_to articles_path 
+    else 
+      render "edit"
+    end
   end
 end
--- alpha-blog/config/routes.rb
+++ alpha-blog/config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root 'pages#index'
-  resources :articles
+  resources :articles 
 end
