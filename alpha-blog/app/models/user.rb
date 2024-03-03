326a31ca3703856816704d0daab5966b2ce6d332
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-03 13:35:05 +0530

    added partials and refactored the code

--- alpha-blog/app/models/user.rb
+++ alpha-blog/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord 
-  has_many :articles
+  # has_many :articles
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum:3,maximum: 50}
   validates :email, presence: true,  uniqueness: { case_sensitive: false }, length: {maximum: 105}
 end
