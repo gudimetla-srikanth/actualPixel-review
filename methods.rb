6933fbb29450d4d88ec9b492f84944a7bd204b60
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-04 17:30:35 +0530

    parameter passing functions

--- methods.rb
+++ methods.rb
@@ -1,31 +1,37 @@
 
-# methods in ruby language are similar to any other languages
+# # methods in ruby language are similar to any other languages
 
-# method without parameters
-def my_print
-    puts "This method is called"
-end
+# # method without parameters
+# def my_print
+#     puts "This method is called"
+# end
 
-my_print()
+# my_print()
 
-#method with parameters
+# #method with parameters
 
-def my_print(name)
-    puts name
-end
-my_print("actual pixel")
+# def my_print(name)
+#     puts name
+# end
+# my_print("actual pixel")
 
-#method with some math init
+# #method with some math init
 
-def multiply(val)
-    return puts val*5
-end
-multiply(4)
+# def multiply(val)
+#     return puts val*5
+# end
+# multiply(4)
 
-#if nothing is returned to method then the end of the expression will be returned atomatically
+# #if nothing is returned to method then the end of the expression will be returned atomatically
 
-def return_method(name)
-    first_name = name +" "+"gudimetla"
-end
-returned_val = return_method("srikanth")
-puts returned_val
+# def return_method(name)
+#     first_name = name +" "+"gudimetla"
+# end
+# returned_val = return_method("srikanth")
+# puts returned_val
+
+#functions with passing parameters without paranthasis
+# def print(myval,val) 
+#   puts "I am printing this value #{myval} and #{val}" 
+# end
+# print 2,:hi
