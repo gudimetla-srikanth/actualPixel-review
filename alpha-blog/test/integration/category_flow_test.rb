44a16ff64bedc5cb926a9c34efc6704614e13a32
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-15 17:52:51 +0530

    alpha-blog project completed

--- alpha-blog/test/integration/category_flow_test.rb
+++ alpha-blog/test/integration/category_flow_test.rb
@@ -2,7 +2,15 @@ require "test_helper"
 
 class CategoryFlowTest < ActionDispatch::IntegrationTest
   test "testing category flow" do 
-    get new_category_path 
+    get '/'
     assert_response :success
+    get '/login'
+    assert_response :success
+    post '/login',params:{user:{email:"srikanth@gmail.com",password:"12345678"}}
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    get new_category_path 
+    assert_response :redirect
   end
 end
