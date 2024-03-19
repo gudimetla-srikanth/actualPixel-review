7ed2b811344b44cecbbfa694e464578e47e5bc23
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-19 14:17:09 +0530

    javascript practice

--- test_app/app/javascript/application.js
+++ test_app/app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+console.log("I am running")
+var element = document.getElementById('new')
+element.style="color:red;cursor:pointer"
