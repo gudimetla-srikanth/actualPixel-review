cb8247cff4aeb88edd9b7be8071166a73c43bab6
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-19 16:48:59 +0530

    practice sample helper actions in test_app

--- test_app/app/controllers/todos_controller.rb
+++ test_app/app/controllers/todos_controller.rb
@@ -1,4 +1,10 @@
 class TodosController < ApplicationController
+  before_action :sever
   def new 
+    render json: @message[:name]
+  end
+  private 
+  def sever
+    @message = {name:"srikanth gudimetla"}
   end
 end
