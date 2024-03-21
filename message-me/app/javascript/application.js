45d0bf9446d8eec795801c4428044de2c273eb4e
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-21 13:24:46 +0530

    added message styling

--- message-me/app/javascript/application.js
+++ message-me/app/javascript/application.js
@@ -7,6 +7,14 @@ import "semantic-ui"
 $(document).on('turbo:load', function() {
   console.log('loaded turbo links')
   $('.ui.dropdown').dropdown()
+  $('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  })
+;
 });
 import "channels"
 
