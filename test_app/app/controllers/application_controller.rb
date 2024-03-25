005e7979a4d3afede115f649620e9e67232229d2
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-25 12:50:29 +0530

    practice

--- test_app/app/controllers/application_controller.rb
+++ test_app/app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
+   before_action :new_method
   def auth
      if session[:id] == nil
         redirect_to signin_path 
      end
+  end
+  def new_method
+   render html: "hi bro! where are you"
   end
 end
