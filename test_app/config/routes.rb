cee2b3bfb83eaa637de87868f5e277bf84908755
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-26 13:46:43 +0530

    added Device gem

--- test_app/config/routes.rb
+++ test_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # root 'todos#new'
   # mount ActionCable.server,at:'/cable'
   root 'articles#index'
--- test_app/db/schema.rb
+++ test_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_23_091449) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_26_081221) do
   create_table "articles", force: :cascade do |t|
     t.string "article_name"
     t.datetime "created_at", null: false
@@ -51,6 +51,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_23_091449) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
