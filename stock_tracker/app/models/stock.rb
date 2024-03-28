4ef87a4ed6ba2b81468a1ff800db1bfd05b98729
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-28 20:46:10 +0530

    added stock find functionality

--- stock_tracker/app/models/stock.rb
+++ stock_tracker/app/models/stock.rb
@@ -3,6 +3,6 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(publishable_token: '',secret_token: '',
     endpoint: 'https://cloud.iexapis.com/v1'
     )
-    client.price(ticker)
+    ticker_obj={ticker:ticker,company:client.company(ticker).company_name,last_price:client.price(ticker)}
   end
 end
