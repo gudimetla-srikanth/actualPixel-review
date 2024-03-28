632d50e5ceadcafe7f1a86837728c4381977f46c
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-28 13:02:34 +0530

    added users and stocks controller to app

--- test_app/config/routes.rb
+++ test_app/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # root 'todos#new'
-  # mount ActionCable.server,at:'/cable'
-  root 'articles#index'
-  resources :articles
-  get 'signin',to:'users#signin'
-  post 'login',to:'users#login'
+  # devise_for :users
+  # # root 'todos#new'
+  # # mount ActionCable.server,at:'/cable'
+  # root 'articles#index'
+  # resources :articles
+  # get 'signin',to:'users#signin'
+  # post 'login',to:'users#login'
+  root "mine#index"
 end
