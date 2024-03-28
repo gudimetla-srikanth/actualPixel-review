cafe2def227fe585f6ba37d00054fe5f34755f07
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-03-28 10:10:55 +0530

    ignoring stock model file

--- stock_tracker/app/models/stock.rb
+++ stock_tracker/app/models/stock.rb
@@ -1,8 +1,6 @@
 class Stock < ApplicationRecord
   def self.new_lookup(ticker) 
-    client = IEX::Api::Client.new(
-    publishable_token: '',
-    secret_token: '',
+    client = IEX::Api::Client.new(publishable_token: '',secret_token: '',
     endpoint: 'https://cloud.iexapis.com/v1'
     )
     client.price(ticker)
