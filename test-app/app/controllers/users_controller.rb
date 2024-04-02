ef912aa1c87d155b28f5dd97bb227f04aad6b039
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-04-02 13:09:02 +0530

    turbo frame tag

--- test-app/app/controllers/users_controller.rb
+++ test-app/app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-def sample1
+def sample
   # @val=1
   # render partial: "result",locals:{val:@val}
   # render partial: "result"
-  @users = User.all
+  # @users = User.all
 end
 def turn
 #  respond_to do |format|
 #   format.js { render partial: 'result.js' }
 #  end
-  # render turbo_stream: turbo_stream.replace("counter",partial: "result")
+  render turbo_stream: turbo_stream.replace("counter",partial: "datas/data")
 end
 end
--- test-app/config/routes.rb
+++ test-app/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root "users#sample1"
+  root "users#sample"
   get '/turn',to:"users#turn"
 end
