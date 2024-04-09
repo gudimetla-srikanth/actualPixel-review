5af897195d2f0fe8e9769fa16caa0fc0506a249f
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-04-09 13:37:27 +0530

    added self referencing

--- stock_tracker/db/schema.rb
+++ stock_tracker/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_09_040508) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_09_075820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,11 +42,35 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_09_040508) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "authors", force: :cascade do |t|
+    t.string "author_name"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "auth"
+  end
+
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "book_name"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "patron_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
   create_table "sampledbs", force: :cascade do |t|
@@ -96,5 +120,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_09_040508) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "friendships", "users"
   add_foreign_key "user_stocks", "stocks"
 end
