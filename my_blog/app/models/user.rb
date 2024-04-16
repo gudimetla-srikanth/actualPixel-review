a3e55bac041be7ec02f212063120a4d0426a903e
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-04-16 15:36:05 +0530

    added validations before fields

--- my_blog/app/models/user.rb
+++ my_blog/app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # <%= render "devise/shared/error_messages", resource: resource%>
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
         
-  has_many :blogs, dependent: :destroy
-        
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email,presence: true,format: {with:VALID_EMAIL_REGEX,message: "is not valid"}
-  # validates :password, presence:true,format: { with: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\Wmessage: " must include at least one number, lowercase letter, uppercase letter, and special character" }
-        
+  has_many :blogs, dependent: :destroy   
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,presence: true,format: {with:VALID_EMAIL_REGEX,message: "is not valid"} 
+  validates :password, presence: true, length: { minimum: 8 }
+                      #  format: { with: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/,
+                      #            message: "must include at least one lowercase letter, one uppercase letter, and one digit" }
+  
 end
