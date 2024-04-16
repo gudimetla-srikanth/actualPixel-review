efad162945731fec825432cd6cc30fa3b7bd5fb1
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-04-16 17:45:51 +0530

    login & signup page edited and added validation errors

--- my_blog/app/models/user.rb
+++ my_blog/app/models/user.rb
@@ -8,8 +8,10 @@ class User < ApplicationRecord
   has_many :blogs, dependent: :destroy   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,presence: true,format: {with:VALID_EMAIL_REGEX,message: "is not valid"} 
-  validates :password, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 8 },confirmation:true
                       #  format: { with: /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/,
                       #            message: "must include at least one lowercase letter, one uppercase letter, and one digit" }
-  
+  validates :password_confirmation,presence: true
+  validates :first_name,presence: true,length:{minimum:3}
+  validates :last_name,presence: true,length:{minimum:2}
 end
