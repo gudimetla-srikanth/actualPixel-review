0b095e27f9ee9421ea947b609b1f690ba37b9560
gudimetla srikanth <gudimetlasrikanth1@gmail.com>
2024-04-24 18:10:20 +0530

    added only specific field of blog model are exported to that CSV file

--- my_blog/app/models/blog.rb
+++ my_blog/app/models/blog.rb
@@ -2,11 +2,12 @@ class Blog < ApplicationRecord
   validates :title,:description,presence: true 
   has_one_attached :image
   belongs_to :user
-  def self.generate_csv 
+  def self.generate_csv
+    attributes = %w[title description]
     CSV.generate do |csv|
-      csv << column_names
+      csv << attributes
       all.each do |record|
-        csv << record.attributes.values
+        csv << record.attributes.values_at(*attributes)
       end
     end
   end
